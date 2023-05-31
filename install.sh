d2479926794d5829bbeb9f41fa2384066ed60619
bombvillIan <38000850+bombvillIan@users.noreply.github.com>
2023-05-31 21:54:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed -i '/m0eak_feeds/d' /etc/opkg/customfeeds.conf
-echo "src/gz m0eak_feeds https://raw.githubusercontent.com/bombvillIan/Openwrt_Packages/main/AXT1800/Packages.gz" >>/etc/opkg/customfeeds.conf
+echo "src/gz m0eak_feeds https://raw.githubusercontent.com/bombvillIan/Openwrt_Packages/main/AXT1800" >>/etc/opkg/customfeeds.conf
 opkg update
 action="$1"
 case "${action}" in
@@ -11,10 +11,10 @@ passwall)
 	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-compat luci-lib-ipkg
 	;;
 openclash)
-	wget -qO- https://raw.githubusercontent.com/gh/m0eak/Install-Openclash-for-Axt1800/install_clash.sh | sh
+	wget -qO- https://raw.githubusercontent.com/bombvillIan/Openwrt_Packages/main/AXT1800/install_clash.sh | sh
 	;;
 *)
 	opkg install luci-i18n-base-zh-cn luci-app-passwall luci-i18n-passwall-zh-cn luci-app-ssr-plus luci-i18n-ssr-plus-zh-cn luci-compat luci-lib-ipkg
-	wget -qO- https://raw.githubusercontent.com/gh/m0eak/Install-Openclash-for-Axt1800/install_clash.sh | sh
+	wget -qO- https://raw.githubusercontent.com/bombvillIan/Openwrt_Packages/main/AXT1800/install_clash.sh | sh
 	;;
 esac
